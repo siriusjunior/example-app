702079a62614ed7b86cf81c96f14fb83fd72b7af
siriusjunior <topdownreading@gmail.com>
2022-09-03 05:40:18 +0900

    [Modify]migrationファイル

--- database/migrations/2022_09_02_203049_add_user_id_to_tweets.php
+++ database/migrations/2022_09_02_203049_add_user_id_to_tweets.php
@@ -14,7 +14,9 @@ return new class extends Migration
     public function up()
     {
         Schema::table('tweets', function (Blueprint $table) {
-            //
+            $table->unsignedBigInteger('user_id')->after('id');
+            // usersテーブルのidの外部キー制約
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
@@ -26,7 +28,8 @@ return new class extends Migration
     public function down()
     {
         Schema::table('tweets', function (Blueprint $table) {
-            //
+            $table->dropForeign('tweets_user_id_foreign');
+            $table->dropColumn('user_id');
         });
     }
 };
