c4b3664a528cc1064ea4a19e59cdcc88a68424a8
siriusjunior <topdownreading@gmail.com>
2022-09-03 05:47:49 +0900

    [Modify]Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call([TweetsSeeder::class]);
+        $this->call([
+            UsersSeeder::class,
+            TweetsSeeder::class
+        ]);
     }
 }
