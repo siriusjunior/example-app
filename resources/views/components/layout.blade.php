4ec69b7c2ea3dd950c4d4c8ea162e4d6d1c9fcea
siriusjunior <topdownreading@gmail.com>
2022-09-05 06:13:08 +0900

    [Add]optionsコンポーネントの作成

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,6 +6,7 @@
   <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>{{ $title ?? 'つぶやきアプリ' }}</title>
+  @stack('css')
   <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
   <script src="{{ mix('/js/app.js') }}"></script>
 </head>
