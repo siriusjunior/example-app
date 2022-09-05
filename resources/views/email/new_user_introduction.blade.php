a6b29e58fee94902aedc55f695d85936fc093202
siriusjunior <topdownreading@gmail.com>
2022-09-05 13:00:33 +0900

    [Add]HTMLメールのスタイリング

--- resources/views/email/new_user_introduction.blade.php
+++ resources/views/email/new_user_introduction.blade.php
@@ -1,5 +1,11 @@
 @component('mail::message')
 
+@component('mail::panel')
 {{ $toUser->name }}さんこんにちは！新しく{{ $newUser->name }}さんが追加されました！
+@endcomponent
+
+@component('mail::button',['url'=> route('tweet.index')])
+つぶやきを見にいく
+@endcomponent
 
 @endcomponent
