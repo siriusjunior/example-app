eaf8b3637d6d4bd6e571bbb4570d72c303fca96b
siriusjunior <topdownreading@gmail.com>
2022-09-06 05:33:49 +0900

    [Modify]SampleCommand

--- app/Console/Commands/SampleCommand.php
+++ app/Console/Commands/SampleCommand.php
@@ -11,22 +11,26 @@ class SampleCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'command:name';
+    // コマンドの名前
+    protected $signature = 'sample-command';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command description';
+    // コマンドの説明文
+    protected $description = 'Sample Command';
 
     /**
      * Execute the console command.
      *
      * @return int
      */
+    // コマンドの実処理
     public function handle()
     {
+        echo 'このコマンドはサンプルです。';
         return 0;
     }
 }
