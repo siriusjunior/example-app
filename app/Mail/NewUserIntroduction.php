e30da5f8c0a202c5ffe3f1db9209e5b47f6a91ce
siriusjunior <topdownreading@gmail.com>
2022-09-05 12:43:59 +0900

    [Add]メールの受信者・送信者データ渡し

--- app/Mail/NewUserIntroduction.php
+++ app/Mail/NewUserIntroduction.php
@@ -2,6 +2,7 @@
 
 namespace App\Mail;
 
+use App\Models\User;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
@@ -12,15 +13,18 @@ class NewUserIntroduction extends Mailable
     use Queueable, SerializesModels;
 
     public $subject = '新しいユーザーが追加されました！';
+    public User $toUser;
+    public User $newUser;
 
     /**
      * Create a new message instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(User $toUser, User $newUser)
     {
-        //
+        $this->toUser = $toUser;
+        $this->newUser = $newUser;
     }
 
     /**
