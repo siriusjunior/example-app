9418e7239785bd8f90ec5195d336b1120aa0145f
siriusjunior <topdownreading@gmail.com>
2022-09-07 05:50:20 +0900

    [Add]つぶやき集計するロジック・集計メール送信コマンド

--- app/Services/TweetService.php
+++ app/Services/TweetService.php
@@ -3,6 +3,7 @@
 namespace App\Services;
 
 use App\Models\Tweet;
+use Carbon\Carbon;
 
 class TweetService
 {
@@ -18,5 +19,12 @@ class TweetService
       return false;
     }
     return $tweet->user_id === $userId;
+  }
+
+  public function countYesterdayTweets(): int
+  {
+    return Tweet::whereDate('created_at', '>=', Carbon::yesterday()->toDateTimeString())
+      ->whereDate('created_at', '<', Carbon::today()->toDateTimeString())
+      ->count();
   }
 }
